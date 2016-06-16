3114590ddcd858ba000ab24bbc982d2aa2d6aff6
Simon Griffiths <sgriffiths@mdsol.com>
2016-06-16 17:24:58 +0100

    Updated version of Kafka

--- install_kafka.sh
+++ install_kafka.sh
@@ -1,10 +1,10 @@
 sudo apt-get update
 sudo apt-get install openjdk-7-jdk -y
 echo "Downloading Kafka, please wait..."
-wget -q http://apache.mirrors.hoobly.com/kafka/0.8.2.1/kafka_2.10-0.8.2.1.tgz
+wget -q http://mirror.catn.com/pub/apache/kafka/0.10.0.0/kafka_2.11-0.10.0.0.tgz
 echo "Download complete, finishing install"
-tar -zxvf kafka_2.10-0.8.2.1.tgz
-cd kafka_2.10-0.8.2.1/
+tar -zxvf kafka_2.11-0.10.0.0.tgz
+cd kafka_2.11-0.10.0.0/
 echo "host.name=192.168.33.10" >> config/server.properties
 bin/zookeeper-server-start.sh config/zookeeper.properties &
 bin/kafka-server-start.sh config/server.properties &
